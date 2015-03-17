59d83c733f01ebdd4c425a19a98a452e15d3dd8c
oleg5991 <oleg5991@gmail.com>
2015-03-17 18:11:11 +0300

    testing

---
+++ Testing/Testing/Testing/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Model
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<string> Roles { get; set; }
+        public bool Enabled { get; set; }
+
+        public User()
+        {
+            UserName = string.Empty;
+            Password = string.Empty;
+
+            FirstName = string.Empty;
+            LastName = string.Empty;
+
+            Roles = new List<string>();
+            Enabled = false;
+        }
+    }
+}
---
+++ Testing/Testing/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+        using System.Collections.Generic;
+        using Microsoft.VisualStudio.TestTools.UnitTesting;
+        using Testing.Model;
+
+        [TestClass]
+        public class UserTests
+        {
+            /// <summary>
+            /// Test to check that the properties are correctly set when the user object is instantiated.
+            /// </summary>
+            [TestMethod]
+            public void Consructor_Test()
+            {
+                User newUser = new User();
+
+                Assert.AreEqual(string.Empty, newUser.UserName, "Should be an empty string");
+                Assert.AreEqual(string.Empty, newUser.Password, "Should be an empty string");
+                Assert.AreEqual(string.Empty, newUser.FirstName, "Should be an empty string");
+                Assert.AreEqual(string.Empty, newUser.LastName, "Should be an empty string");
+
+                Assert.IsFalse(newUser.Enabled, "Should be false");
+
+                Assert.IsNotNull(newUser.Roles, "Needs to be instantiated");
+                Assert.IsInstanceOfType(newUser.Roles, typeof(List<string>), "Should be 'List<string>'");
+                Assert.AreEqual(0, newUser.Roles.Count, "Should be an empty collection");
+            }
+
+            [TestMethod]
+            public void Consructor_Test_2()
+            {
+                User newUser = new User();
+
+                Assert.AreEqual(string.Empty, newUser.UserName, "Should be an empty string");
+                Assert.AreEqual(string.Empty, newUser.Password, "Should be an empty string");
+                Assert.AreEqual(string.Empty, newUser.FirstName, "Should be an empty string");
+                Assert.AreNotEqual(string.Empty, newUser.LastName, "Should be an empty string");
+
+                Assert.IsFalse(newUser.Enabled, "Should be false");
+
+                Assert.IsNotNull(newUser.Roles, "Needs to be instantiated");
+                Assert.IsInstanceOfType(newUser.Roles, typeof(List<string>), "Should be 'List<string>'");
+                Assert.AreEqual(0, newUser.Roles.Count, "Should be an empty collection");
+            }
+        }
+}
