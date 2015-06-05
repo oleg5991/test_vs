76caaaf4a9e3a81c7806530baadfaef535d04374
oleg5991 <oleg5991@gmail.com>
2015-06-05 16:14:53 +0300

    Update run1.bat

--- Testing/run1.bat
+++ Testing/run1.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv" /build Debug "C:\Users\oleg5_000\Desktop\Testing\Testing\Testing.sln"
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console" "C:\Users\oleg5_000\Desktop\Testing\Testing\UnitTestProject\bin\Debug\UnitTestProject.dll
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv" /build Debug "test_vs/Testing/Testing/Testing.sln"
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console" "test_vs/Testing/Testing/UnitTestProject/bin/Debug/UnitTestProject.dll"
